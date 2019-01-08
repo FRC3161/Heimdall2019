71e8bd37cdd6de8acfb212d7ac94a91b40218ed8
Tronic Titans <Tronic Titans@DESKTOP-386EC4S>
2019-01-08 16:29:55 -0500

    Test commit from driver station

--- Heimdall/src/main/java/frc/robot/Robot.java
+++ Heimdall/src/main/java/frc/robot/Robot.java
@@ -32,7 +32,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+    SmartDashboard.putData("Auto choices pls", m_chooser);
   }
 
   /**
