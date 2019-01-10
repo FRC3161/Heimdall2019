1f4873ffd219de375edcc555e7a7a5c85d15c311
Andrew Azores <me@andrewazor.es>
2019-01-10 01:01:17 -0500

    Add CTRE libs and switch to TalonSRXs

--- Heimdall/src/main/java/frc/robot/subsystems/DriveImpl.java
+++ Heimdall/src/main/java/frc/robot/subsystems/DriveImpl.java
@@ -1,23 +1,21 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import frc.robot.RobotMap;
-import edu.wpi.first.wpilibj.Talon;
-
-//TODO: Change it from the wpi talons to the CTRE
 
 public class DriveImpl implements Drive {
     private final MecanumDrive drivetrain;
-    private final Talon frontLeftDrive;
-    private final Talon frontRightDrive;
-    private final Talon backLeftDrive;
-    private final Talon backRightDrive;
+    private final WPI_TalonSRX frontLeftDrive;
+    private final WPI_TalonSRX frontRightDrive;
+    private final WPI_TalonSRX backLeftDrive;
+    private final WPI_TalonSRX backRightDrive;
 
     public DriveImpl() {
-        this.frontLeftDrive = new Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
-        this.frontRightDrive = new Talon(RobotMap.DRIVETRAIN_LEFT_BACK_TALON);
-        this.backLeftDrive = new Talon(RobotMap.DRIVETRAIN_RIGHT_FRONT_TALON);
-        this.backRightDrive = new Talon(RobotMap.DRIVETRAIN_RIGHT_BACK_TALON);
+        this.frontLeftDrive = new WPI_TalonSRX(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
+        this.frontRightDrive = new WPI_TalonSRX(RobotMap.DRIVETRAIN_LEFT_BACK_TALON);
+        this.backLeftDrive = new WPI_TalonSRX(RobotMap.DRIVETRAIN_RIGHT_FRONT_TALON);
+        this.backRightDrive = new WPI_TalonSRX(RobotMap.DRIVETRAIN_RIGHT_BACK_TALON);
 
         this.drivetrain = new MecanumDrive(frontLeftDrive, frontRightDrive, backLeftDrive, backRightDrive);
     }
