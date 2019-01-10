a79df53eb81274193f79451cc5a28f3592412784
Andrew Azores <me@andrewazor.es>
2019-01-10 00:01:00 -0500

    Formatting

--- Heimdall/src/main/java/frc/robot/subsystems/Drive.java
+++ Heimdall/src/main/java/frc/robot/subsystems/Drive.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.Talon;
 
 //TODO: Change it from the wpi talons to the CTRE
 
-public class Drive{
+public class Drive {
     private MecanumDrive drivetrain;
     private Talon frontLeftDrive;
     private Talon frontRightDrive;
@@ -15,22 +15,17 @@ public class Drive{
 
     RobotMap obj = new RobotMap();
 
-
-
     public void driveInit() {
+        this.frontLeftDrive = new Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
+        this.frontRightDrive = new Talon(RobotMap.DRIVETRAIN_LEFT_BACK_TALON);
+        this.backLeftDrive = new Talon(RobotMap.DRIVETRAIN_RIGHT_FRONT_TALON);
+        this.backRightDrive = new Talon(RobotMap.DRIVETRAIN_RIGHT_BACK_TALON);
 
-
-        frontLeftDrive = new Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
-        frontRightDrive = new Talon(RobotMap.DRIVETRAIN_LEFT_BACK_TALON);
-        backLeftDrive = new Talon(RobotMap.DRIVETRAIN_RIGHT_FRONT_TALON);
-        backRightDrive = new Talon(RobotMap.DRIVETRAIN_RIGHT_BACK_TALON);
-
-
-        drivetrain = new MecanumDrive(frontLeftDrive, frontRightDrive, backLeftDrive, backRightDrive);
+        this.drivetrain = new MecanumDrive(frontLeftDrive, frontRightDrive, backLeftDrive, backRightDrive);
     }
 
     public void driveRobot(double leftStickX, double leftStickY, double rightStickX) {
-        drivetrain.driveCartesian(leftStickX, leftStickY, rightStickX);
+        this.drivetrain.driveCartesian(leftStickX, leftStickY, rightStickX);
     }
 
 }
