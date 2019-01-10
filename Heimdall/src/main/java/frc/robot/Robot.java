8af164677886b461f57079f1995d1e89d02d9620
Andrew Azores <me@andrewazor.es>
2019-01-10 00:00:05 -0500

    Remove unused import

--- Heimdall/src/main/java/frc/robot/Robot.java
+++ Heimdall/src/main/java/frc/robot/Robot.java
@@ -10,7 +10,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.subsystems.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
