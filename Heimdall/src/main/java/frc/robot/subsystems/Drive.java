d62730201e9615c00c2400d515e550f5f0817c1e
Andrew Azores <me@andrewazor.es>
2019-01-10 00:00:31 -0500

    Drive does not need to know about Robot

--- Heimdall/src/main/java/frc/robot/subsystems/Drive.java
+++ Heimdall/src/main/java/frc/robot/subsystems/Drive.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
-import frc.robot.Robot;
 import frc.robot.RobotMap;
 import edu.wpi.first.wpilibj.Talon;
 
@@ -16,11 +15,11 @@ public class Drive{
 
     RobotMap obj = new RobotMap();
 
-    
-    
+
+
     public void driveInit() {
-        
-        
+
+
         frontLeftDrive = new Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
         frontRightDrive = new Talon(RobotMap.DRIVETRAIN_LEFT_BACK_TALON);
         backLeftDrive = new Talon(RobotMap.DRIVETRAIN_RIGHT_FRONT_TALON);
