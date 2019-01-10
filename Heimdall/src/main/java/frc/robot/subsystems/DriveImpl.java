ad16d4937d135ed71f92cb8657562f8a81d597dc
Andrew Azores <me@andrewazor.es>
2019-01-10 00:04:08 -0500

    Add interface

---
+++ Heimdall/src/main/java/frc/robot/subsystems/DriveImpl.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import frc.robot.RobotMap;
+import edu.wpi.first.wpilibj.Talon;
+
+//TODO: Change it from the wpi talons to the CTRE
+
+public class DriveImpl implements Drive {
+    private final MecanumDrive drivetrain;
+    private final Talon frontLeftDrive;
+    private final Talon frontRightDrive;
+    private final Talon backLeftDrive;
+    private final Talon backRightDrive;
+
+    public DriveImpl() {
+        this.frontLeftDrive = new Talon(RobotMap.DRIVETRAIN_LEFT_FRONT_TALON);
+        this.frontRightDrive = new Talon(RobotMap.DRIVETRAIN_LEFT_BACK_TALON);
+        this.backLeftDrive = new Talon(RobotMap.DRIVETRAIN_RIGHT_FRONT_TALON);
+        this.backRightDrive = new Talon(RobotMap.DRIVETRAIN_RIGHT_BACK_TALON);
+
+        this.drivetrain = new MecanumDrive(frontLeftDrive, frontRightDrive, backLeftDrive, backRightDrive);
+    }
+
+    public void drive(double forwardRate, double strafeRate, double turnRate) {
+        this.drivetrain.driveCartesian(forwardRate, strafeRate, turnRate);
+    }
+}
+
