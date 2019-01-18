54d22753757c3773b3fae907b4d789889599b56a
David Drobner <david@drobner.ca>
2019-01-18 17:52:36 -0500

    Added new constructor for OmniPodImpl which allows for new PID values to be set

--- src/main/java/frc/robot/subsystems/PIDImpl.java
+++ src/main/java/frc/robot/subsystems/PIDImpl.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import frc.robot.subsystems.TalonPIDSource;
+import frc.robot.subsystems.drivetrain.OmniPod;
+import frc.robot.subsystems.drivetrain.OmniPodImpl;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDOutput;
@@ -17,11 +19,12 @@ public class PIDImpl implements PID {
     double Kd; 
 
     public PIDImpl() {
+        //TODO Do this for all encoders (currently only for FL, will rename later)
         this.Kp = 0.001;
         this.Ki = 0.001;
         this.Kd = 0.001;
 
-        //this.talonPID = new TalonPIDSource()
+        //this.talonPID = new TalonPIDSource();
         
         this.pidcontrol = new PIDController(Kp, Ki, Kd, talonPID, pidoutput);
     }
