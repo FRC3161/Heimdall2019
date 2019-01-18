9da571b23722d79c1d0e91c3d31aafe1d3337ca4
David Drobner <david@drobner.ca>
2019-01-18 14:22:28 -0500

    Added pidcontroller declaration

--- src/main/java/frc/robot/subsystems/PIDImpl.java
+++ src/main/java/frc/robot/subsystems/PIDImpl.java
@@ -2,19 +2,27 @@ package frc.robot.subsystems;
 
 import frc.robot.subsystems.TalonPIDSource;
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDOutput;
 
 public class PIDImpl implements PID {
 
-    double rotate;
-
     PIDController pidcontrol;
+    PIDSource pidsource;
+    PIDOutput pidoutput;
     
     double Kp;
     double Ki;
     double Kd; 
 
-     @Override
-    public void gyroPidWrite(double output) {
-        rotate = output;
+    @Override
+    public void PIDInit() {
+        this.Kp = 0.001;
+        this.Ki = 0.001;
+        this.Kd = 0.001;
+        
+        this.pidcontrol = new PIDController(Kp, Ki, Kd, pidsource, pidoutput);
     }
+
+
 }
