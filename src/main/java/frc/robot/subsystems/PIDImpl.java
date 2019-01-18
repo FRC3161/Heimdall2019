25bd6548c49faf88071ed6b7f2ff3835ba11e711
David Drobner <david@drobner.ca>
2019-01-17 23:17:56 -0500

    Moved old PID from Drive into new PID subsystem

--- src/main/java/frc/robot/subsystems/PIDImpl.java
+++ src/main/java/frc/robot/subsystems/PIDImpl.java
@@ -1,5 +1,10 @@
 package frc.robot.subsystems;
 
 public class PIDImpl implements PID {
+     
+    //Setting PID Values so they aren't empty
+     this.Kp = 0.001;
+     this.Ki = 0.001;
+     this.Kd = 0.001;
 
 }
