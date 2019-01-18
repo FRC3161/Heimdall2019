9ca228b9e903eeecac865012f911d5ddda14c954
David Drobner <david@drobner.ca>
2019-01-18 18:15:10 -0500

    Implemented PIDController in OmniPodImpl

--- src/main/java/frc/robot/subsystems/drivetrain/OmniPodImpl.java
+++ src/main/java/frc/robot/subsystems/drivetrain/OmniPodImpl.java
@@ -1,14 +1,43 @@
 package frc.robot.subsystems.drivetrain;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.PIDController;
+import frc.robot.subsystems.TalonPIDSource;
 
 // TODO implement PID control over the contained Talon with its attached encoder
 public class OmniPodImpl implements OmniPod {
 
     private final WPI_TalonSRX talon;
+    
+    //PID
+    private double Kp;
+    private double Ki;
+    private double Kd;
+
+    TalonPIDSource talonPIDSource;
+    PIDController talonPIDController;
+    //PIDOutput talonPIDOutput;
 
     public OmniPodImpl(int talonCANPort) {
+        this.Kp = 0.001;
+        this.Ki = 0.001;
+        this.Kd = 0.001;
+        
         this.talon = new WPI_TalonSRX(talonCANPort);
+
+        this.talonPIDSource = new TalonPIDSource(talon);
+        this.talonPIDController = new PIDController(Kp, Ki, Kd, talonPIDSource, talon);
+    }
+
+    public OmniPodImpl(int talonCANPort, double Kp, double Ki, double Kd){
+        this.Kp = Kp;
+        this.Ki = Ki;
+        this.Kd = Kd;
+        
+        this.talon = new WPI_TalonSRX(talonCANPort);
+
+        this.talonPIDSource = new TalonPIDSource(talon);
+        this.talonPIDController = new PIDController(Kp, Ki, Kd, talonPIDSource, talon);
     }
 
     @Override
