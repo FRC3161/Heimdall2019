bfadbb1f0a5cc20fa0338cafb259daf3a5b771fc
Andrew Azores <me@andrewazor.es>
2019-01-19 10:44:48 -0500

    Fix formatting

--- src/main/java/frc/robot/subsystems/TalonPIDSource.java
+++ src/main/java/frc/robot/subsystems/TalonPIDSource.java
@@ -7,42 +7,42 @@ import java.util.function.Function;
 
 public class TalonPIDSource implements PIDSource {
     private final WPI_TalonSRX talon;
-	    private final int sensorIndex;
-	    private PIDSourceType type = PIDSourceType.kDisplacement;
-	
-	    public TalonPIDSource(final WPI_TalonSRX talon, final int sensorIndex) {
-	        this.talon = talon;
-	        this.sensorIndex = sensorIndex;
-	    }
-	
-	    public TalonPIDSource(final WPI_TalonSRX talon) {
-	        this(talon, 0);
-	    }
-	
-	    @Override
-	    public void setPIDSourceType(PIDSourceType pidSourceType) {
-	        this.type = pidSourceType;
-	    }
-	
-	    @Override
-	    public PIDSourceType getPIDSourceType() {
-	        return this.type;
-	    }
-	
-	    @Override
-	    public double pidGet() {
-	        final Function<Integer, Integer> producer;
-	        switch (this.type) {
-	            case kRate:
-	                producer = this.talon::getSelectedSensorVelocity;
-	                break;
-	            case kDisplacement:
-	                producer = this.talon::getSelectedSensorPosition;
-	                break;
-	            default:
-	                producer = (idx) -> 0;
-	                break;
-	        }
-	        return producer.apply(this.sensorIndex);
-}
+    private final int sensorIndex;
+    private PIDSourceType type = PIDSourceType.kDisplacement;
+
+    public TalonPIDSource(final WPI_TalonSRX talon, final int sensorIndex) {
+        this.talon = talon;
+        this.sensorIndex = sensorIndex;
+    }
+
+    public TalonPIDSource(final WPI_TalonSRX talon) {
+        this(talon, 0);
+    }
+
+    @Override
+    public void setPIDSourceType(PIDSourceType pidSourceType) {
+        this.type = pidSourceType;
+    }
+
+    @Override
+    public PIDSourceType getPIDSourceType() {
+        return this.type;
+    }
+
+    @Override
+    public double pidGet() {
+        final Function<Integer, Integer> producer;
+        switch (this.type) {
+            case kRate:
+                producer = this.talon::getSelectedSensorVelocity;
+                break;
+            case kDisplacement:
+                producer = this.talon::getSelectedSensorPosition;
+                break;
+            default:
+                producer = (idx) -> 0;
+                break;
+        }
+        return producer.apply(this.sensorIndex);
+    }
 }
