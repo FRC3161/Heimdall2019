ff4e1378c802c8b044778aa940ebf01733c83c02
David Drobner <david@drobner.ca>
2019-01-23 21:22:17 -0500

    Created Skeleton ColsonPodImpl

---
+++ src/main/java/frc/robot/subsystems/drivetrain/ColsonPodImpl.java
@@ -0,0 +1,46 @@
+public class ColsonPodImpl implements ColsonPod {
+    
+    ColsonPodImpl(){
+        //TODO
+    }
+    
+    @Override
+    public void setDeployed(boolean deployed) {
+        //TODO
+    }
+
+    @Override
+    public void set(double speed) {
+        //TODO
+    }
+
+    @Override
+    public double get() {
+        //TODO
+    }
+
+    @Override
+    public void disable() {
+        //TODO
+    }
+
+    @Override
+    public void stopMotor() {
+        //TODO
+    }
+
+    @Override
+    public void setInverted(boolean isInverted) {
+        //TODO
+    }
+
+    @Override
+    public boolean getInverted() {
+        //TODO
+    }
+
+    @Override
+    public void pidWrite(double output) {
+        //TODO
+    }
+}
