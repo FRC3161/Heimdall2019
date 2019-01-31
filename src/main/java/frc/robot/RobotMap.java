e93fb8fda2b7c2fdf658e3301e2811201b7966fe
Team 3161 <Tronic Titans@DESKTOP-386EC4S>
2019-01-31 18:48:47 -0500

    Update Talon CAN IDs

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -5,9 +5,9 @@ package frc.robot;
 public class RobotMap {
 
     // Talons
-    public static final int DRIVETRAIN_LEFT_FRONT_TALON = 0;
-    public static final int DRIVETRAIN_LEFT_BACK_TALON = 2;
-    public static final int DRIVETRAIN_RIGHT_FRONT_TALON = 1;
-    public static final int DRIVETRAIN_RIGHT_BACK_TALON = 3;
+    public static final int DRIVETRAIN_LEFT_FRONT_TALON = 1;
+    public static final int DRIVETRAIN_LEFT_BACK_TALON = 3;
+    public static final int DRIVETRAIN_RIGHT_FRONT_TALON = 2;
+    public static final int DRIVETRAIN_RIGHT_BACK_TALON = 4;
 
 }
