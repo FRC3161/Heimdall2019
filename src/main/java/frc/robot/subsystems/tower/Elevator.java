2bc194b248e82b4a32c8c7c98f5b7d52b2c0f1c5
Team 3161 <Tronic Titans@DESKTOP-386EC4S>
2019-02-02 11:25:41 -0500

    Add Elevator subsystem

---
+++ src/main/java/frc/robot/subsystems/tower/Elevator.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.tower;
+
+import frc.robot.subsystems.tower.Tower.Position;
+
+interface Elevator {
+    void setPosition(Position position);
+    Position getPosition();
+    void setEnabled(boolean enabled);
+    boolean isEnabled();
+}
