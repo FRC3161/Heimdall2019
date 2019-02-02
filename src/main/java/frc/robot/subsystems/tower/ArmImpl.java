568e173a60f67e96f1538870ecd8cac766b553a5
Team 3161 <Tronic Titans@DESKTOP-386EC4S>
2019-02-02 12:01:43 -0500

    Add controllers

--- src/main/java/frc/robot/subsystems/tower/ArmImpl.java
+++ src/main/java/frc/robot/subsystems/tower/ArmImpl.java
@@ -1,8 +1,17 @@
 package frc.robot.subsystems.tower;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import frc.robot.subsystems.tower.Tower.Position;
 
 class ArmImpl implements Arm {
+
+    private final TalonSRX controller;
+
+    ArmImpl(int talonPort) {
+        this.controller = new TalonSRX(talonPort);
+    }
+
     @Override
     public void setPosition(Position position) {
         //TODO
--- src/main/java/frc/robot/subsystems/tower/ElevatorImpl.java
+++ src/main/java/frc/robot/subsystems/tower/ElevatorImpl.java
@@ -1,8 +1,17 @@
 package frc.robot.subsystems.tower;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import frc.robot.subsystems.tower.Tower.Position;
 
 class ElevatorImpl implements Elevator {
+
+    private final TalonSRX controller;
+
+    ElevatorImpl(int talonPort) {
+        this.controller = new TalonSRX(talonPort);
+    }
+
     @Override
     public void setPosition(Position position) {
         //TODO
