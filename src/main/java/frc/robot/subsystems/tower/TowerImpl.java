ef6b2eb14e129c621f1a899cf6eecb7f67003d3a
David Drobner <david@drobner.ca>
2019-02-06 23:06:00 -0500

    Fleshed out setClawOpen in TowerImpl

--- src/main/java/frc/robot/subsystems/tower/TowerImpl.java
+++ src/main/java/frc/robot/subsystems/tower/TowerImpl.java
@@ -2,15 +2,19 @@ package frc.robot.subsystems.tower;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.tower.Elevator;
+import edu.wpi.first.wpilibj.Solenoid;
+
 public class TowerImpl implements Tower {
 
     private final Elevator elevator;
     private final Arm arm;
     private Position position;
+    private Solenoid solenoid;
 
     public TowerImpl() {
         this.elevator = new ElevatorImpl(8);
         this.arm = new ArmImpl(7);
+        this.solenoid = solenoid;
         setTowerPosition(Position.STARTING_CONFIG);
     }
 
@@ -29,7 +33,8 @@ public class TowerImpl implements Tower {
 
     @Override
     public void setClawOpen(boolean open) {
-        //TODO
+        solenoid.set(open);
+        //TODO might need a SpeedController? (see setDeployed in ColsonPodImpl)
     }
 
     @Override
