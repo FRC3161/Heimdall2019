74af83ebabd4bcaee455d7e21e50830e227d1beb
Andrew Azores <me@andrewazor.es>
2019-02-09 11:24:27 -0500

    Add Apache commons-collections and use BidiMap for Roller Directions

--- src/main/java/frc/robot/subsystems/tower/TowerImpl.java
+++ src/main/java/frc/robot/subsystems/tower/TowerImpl.java
@@ -2,11 +2,26 @@ package frc.robot.subsystems.tower;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.tower.Elevator;
+
+import org.apache.commons.collections4.BidiMap;
+import org.apache.commons.collections4.bidimap.DualHashBidiMap;
+import org.apache.commons.collections4.bidimap.UnmodifiableBidiMap;
+
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.VictorSP;
 
 public class TowerImpl implements Tower {
+
+    private static final BidiMap<Direction, Double> ROLLER_DIRECTIONS_PWM;
+    static {
+        final BidiMap<Direction, Double> rollerPwms = new DualHashBidiMap<>();
+        // Direction to Roller PWM mapping
+        rollerPwms.put(Direction.NONE, Double.valueOf(0));
+        rollerPwms.put(Direction.IN, Double.valueOf(1));
+        rollerPwms.put(Direction.OUT, Double.valueOf(-1));
+        ROLLER_DIRECTIONS_PWM = UnmodifiableBidiMap.unmodifiableBidiMap(rollerPwms);
+    }
 
     private final Elevator elevator;
     private final Arm arm;
@@ -37,8 +52,8 @@ public class TowerImpl implements Tower {
 
     @Override
     public void setClawOpen(boolean open) {
-        claw.set(open);
         //TODO might need a SpeedController? (see setDeployed in ColsonPodImpl)
+        claw.set(open);
     }
 
     @Override
@@ -59,33 +74,26 @@ public class TowerImpl implements Tower {
 
     @Override
     public void setRollers(Direction direction) {
-        if (direction.equals(Direction.IN)) {
-            roller.set(1.0);
-          } else if (direction.equals(Direction.OUT)) {
-            roller.set(-1.0);
-          } else {
-            roller.set(0.0);
-          }
+        if (!ROLLER_DIRECTIONS_PWM.containsKey(direction)) {
+            roller.set(0);
+            return;
+        }
+        roller.set(ROLLER_DIRECTIONS_PWM.get(direction));
     }
 
     @Override
     public Direction getRollerDirection() {
         double rollerSpeed = roller.get();
-        if (rollerSpeed > 0) {
-          return Direction.IN;
-        }
-        else if (rollerSpeed < 0) {
-            return Direction.OUT;
-        }
-        else {
+        if (!ROLLER_DIRECTIONS_PWM.inverseBidiMap().containsKey(rollerSpeed)) {
             return Direction.NONE;
         }
+        return ROLLER_DIRECTIONS_PWM.inverseBidiMap().get(rollerSpeed);
     }
 
     public void setElevatorSpeed(double speed) {
         this.elevator.setSpeed(speed);
     }
-    
+
     public void setArmSpeed(double speed){
         this.arm.setSpeed(speed);
     }
