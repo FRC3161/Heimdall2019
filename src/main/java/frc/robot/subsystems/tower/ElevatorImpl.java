79326f0390b90137ce1c4ab962ce965275642099
Pieter Baerts <pieterbaerts2001@gmail.com>
2019-02-13 12:24:59 -0500

    added in limit switches

--- src/main/java/frc/robot/subsystems/tower/ElevatorImpl.java
+++ src/main/java/frc/robot/subsystems/tower/ElevatorImpl.java
@@ -2,8 +2,10 @@ package frc.robot.subsystems.tower;
 
 import static frc.robot.MathUtils.absClamp;
 
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.subsystems.tower.Tower.Position;
 
 class ElevatorImpl implements Elevator {
@@ -14,6 +16,8 @@ class ElevatorImpl implements Elevator {
     ElevatorImpl(int masterPort, int slavePort) {
         this.controllerMaster = new WPI_TalonSRX(masterPort);
         this.controllerSlave = new WPI_TalonSRX(slavePort);
+        this.limitSwitchTop = new DigitalInput(1);
+        this.limitSwitchBottom = new DigitalInput(2);
     }
 
     @Override
@@ -30,6 +34,17 @@ class ElevatorImpl implements Elevator {
     @Override
     public void setSpeed(double speed) {
         double maxPower= 0.25;
+        if (LimitSwitchTop = true){
+            maxPower = 0;
+        }
+        elif (limitSwitchBottom = true){
+            if (speed < 0){
+                speed = 0 
+            }
+            else{
+                speed = speed
+            }
+        }
         controllerMaster.set(absClamp(speed, maxPower));
         controllerSlave.set(absClamp(speed, maxPower));
     }
