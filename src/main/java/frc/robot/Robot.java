92f8a5edb526ea06ca3fc3e0c4e7a1ae0e0c2e4e
Pieter Baerts <pieterbaerts2001@gmail.com>
2019-02-18 20:29:58 -0500

    added the staments back in

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -176,9 +176,16 @@ public class Robot extends TitanBot {
     this.driverPad.bind(LogitechButton.RIGHT_BUMPER, x -> this.drive.setFieldCentric(!x));
     this.driverPad.bind(LogitechButton.START, this.drive::resetGyro);
     this.operatorPad.bind(LogitechButton.START, this.tower::reset);
-    this.operatorPad.map(LogitechControl.LEFT_STICK, LogitechAxis.Y, this.tower :: setElevatorSpeed);
-    this.operatorPad.map(LogitechControl.RIGHT_STICK, LogitechAxis.Y, this.tower :: setArmSpeed);
-
+    this.operatorPad.map(LogitechControl.LEFT_STICK, LogitechAxis.Y, x-> {
+      if (x!= 0) {
+        this.tower.setElevatorSpeed(x);
+      } 
+    });
+    this.operatorPad.map(LogitechControl.RIGHT_STICK, LogitechAxis.Y, x-> {
+      if (x!= 0) {
+        this.tower.setArmSpeed(x);
+      } 
+    });
     this.operatorPad.bind(LogitechButton.A, PressType.PRESS, () -> tower.setClawOpen(!tower.isClawOpen()));
     this.operatorPad.bind(LogitechButton.B,() -> tower.setBeakOpen(!tower.isBeakOpen()));
     this.underLay.set(Value.kOn);
