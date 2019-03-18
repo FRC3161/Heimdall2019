d8f84a6430d161153070c5d58c53a725669634e5
Pieter Baerts <pieterbaerts2001@gmail.com>
2019-03-18 18:57:53 -0400

    readed manual arm controll

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -179,11 +179,11 @@ public class Robot extends TitanBot {
         this.tower.setWristSpeed(x);
       }
     });
-    // this.operatorPad.map(LogitechControl.LEFT_STICK, LogitechAxis.Y, x-> {
-    //   if (x!= 0) {
-    //     this.tower.setArmSpeed(x);
-    //   }
-    // });
+    this.operatorPad.map(LogitechControl.LEFT_STICK, LogitechAxis.Y, x-> {
+      if (x!= 0) {
+        this.tower.setArmSpeed(x);
+      }
+    });
     this.operatorPad.bind(LogitechButton.A, PressType.PRESS, this.tower::toggleClaw);
     this.underLay.set(Value.kOn);
 
