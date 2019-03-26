84d039b67e99adc14ad465d647eb59827e80309c
Andrew Azores <me@andrewazor.es>
2019-03-25 21:08:59 -0400

    Remove unused limit switches

--- src/main/java/frc/robot/subsystems/tower/ManualElevatorImpl.java
+++ src/main/java/frc/robot/subsystems/tower/ManualElevatorImpl.java
@@ -5,22 +5,16 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import ca.team3161.lib.robot.LifecycleEvent;
 import ca.team3161.lib.utils.Utils;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.subsystems.tower.Tower.Position;
 
 public class ManualElevatorImpl implements Elevator {
 
     private final WPI_TalonSRX controllerMaster;
     private final WPI_TalonSRX controllerSlave;
-    private final DigitalInput limitSwitchTop;
-    private final DigitalInput limitSwitchBottom;
 
     ManualElevatorImpl(int masterPort, int slavePort, int topSwitchPort , int bottomSwitchPort) {
         this.controllerMaster = Utils.safeInit("elevator controllerMaster", () -> new WPI_TalonSRX(masterPort));
         this.controllerSlave = Utils.safeInit("elevator controllerSlave", () -> new WPI_TalonSRX(slavePort));
-        this.limitSwitchBottom = Utils.safeInit("elevator limitSwitchBottom", () -> new DigitalInput(bottomSwitchPort));
-        this.limitSwitchTop = Utils.safeInit("elevator limitSwitchTop", () -> new DigitalInput(topSwitchPort));
         this.controllerSlave.follow(controllerMaster);
 
         controllerMaster.setInverted(false);
